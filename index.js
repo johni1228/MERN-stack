f33f578121332959f4009614b142955d83b3c0c1
johni1228 <akirus1228@gmail.com>
2021-08-08 22:05:03 -0700

    finish completely

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * Entry Script
  */
-
+   
 if (process.env.NODE_ENV === 'production') {
   process.env.webpackAssets = JSON.stringify(require('./dist/client/manifest.json'));
   process.env.webpackChunkAssets = JSON.stringify(require('./dist/client/chunk-manifest.json'));
